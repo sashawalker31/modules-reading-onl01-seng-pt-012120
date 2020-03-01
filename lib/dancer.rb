f60b308aefec454847efd51632dd718fd58a941f
sasha walker <sashawalker31@gmail.com>
2020-03-01 03:20:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,8 @@
-class Dancer 
-   include Dance
- 
+class Dancer
+  include Dance       #=> "include" - These are instance methods
+  extend MetaDancing  #=> "extend"  - These are class methods
   attr_accessor :name
- 
   def initialize(name)
     @name = name
   end
-end 
+end
