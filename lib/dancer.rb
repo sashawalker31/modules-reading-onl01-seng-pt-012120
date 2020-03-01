0829933d70205ea518259758008e63945ab356ab
sasha walker <sashawalker31@gmail.com>
2020-03-01 03:43:27 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,3 +1,6 @@
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
+
 class Dancer
   include Dance       #=> "include" - These are instance methods
   extend MetaDancing  #=> "extend"  - These are class methods
