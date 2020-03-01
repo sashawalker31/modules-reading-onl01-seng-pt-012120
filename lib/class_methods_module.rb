626c417ef6b96e31a38c0e6dc5ad45dbcfc75948
sasha walker <sashawalker31@gmail.com>
2020-03-01 03:25:45 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,12 +1,5 @@
 module MetaDancing
   
-  class Dancer 
-    extend MetaDancing
-  end 
-  
-  class Kid 
-    extend MetaDancing
-  end 
  
   def metadata
     "This class produces objects that love to dance."
