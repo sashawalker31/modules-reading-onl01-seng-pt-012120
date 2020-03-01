3c0b360075ad4a67e2204d8e37264c63e4f2c7fa
sasha walker <sashawalker31@gmail.com>
2020-03-01 03:20:44 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,8 +1,9 @@
-class Kid 
+class Kid
   include Dance
- 
+  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
   attr_accessor :name
- 
   def initialize(name)
     @name = name
   end
